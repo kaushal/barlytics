cbd2182659f65566dd181e3ec8251f91bff792dc
Kaushal Parikh <kaushalp88@gmail.com>
2013-11-22 22:08:20 +0000

    added analytics page

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/analytics')
+def analytics():
+    return render_template('analytics.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=3000)
